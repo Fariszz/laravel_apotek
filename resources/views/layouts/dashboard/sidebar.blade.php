144788ba0b1180ffc3d0a8c12d71d8548ef36075
Muhammad Faris Hadi Mulyo <ziraflyo@gmail.com>
2022-06-16 10:12:58 +0700

    fix siderbar

--- resources/views/layouts/dashboard/sidebar.blade.php
+++ resources/views/layouts/dashboard/sidebar.blade.php
@@ -2,7 +2,7 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="dashboard.html">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/">
         <div class="sidebar-brand-icon">
             <i class="fas fa-laptop-medical"></i>
         </div>
@@ -23,45 +23,45 @@
     <hr class="sidebar-divider">
 
     @if(Auth::user()->role_id == 1)
-        <!-- Nav Item - User Menu -->
-        <li class="nav-item">
-            <a class="nav-link" href="{{ route('users.index') }}">
-                <i class="fas fa-fw fa-user"></i>
-                <span>User</span></a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="{{ route('payment.check') }}">
-                <i class="fas fa-fw fa-users"></i>
-                <span>User Payment</span></a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="{{ route('category.index') }}">
-                <i class="fas fa-fw fa-box"></i>
-                <span>Category</span></a>
-        </li>
-        <!-- Nav Item - Dropshipper Menu -->
-        {{-- <li class="nav-item">
-            <a class="nav-link" href="dropshipper.html">
-                <i class="fab fa-fw fa-dropbox"></i>
-                <span>Dropshipper</span></a>
-        </li> --}}
+    <!-- Nav Item - User Menu -->
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('users.index') }}">
+            <i class="fas fa-fw fa-user"></i>
+            <span>User</span></a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('payment.check') }}">
+            <i class="fas fa-fw fa-users"></i>
+            <span>User Payment</span></a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('category.index') }}">
+            <i class="fas fa-fw fa-box"></i>
+            <span>Category</span></a>
+    </li>
+    <!-- Nav Item - Dropshipper Menu -->
+    {{-- <li class="nav-item">
+        <a class="nav-link" href="dropshipper.html">
+            <i class="fab fa-fw fa-dropbox"></i>
+            <span>Dropshipper</span></a>
+    </li> --}}
     @endif
 
     @if(Auth::user()->role_id == 1 || Auth::user()->role_id == 2)
-        <!-- Nav Item - Items Menu -->
-        <li class="nav-item">
-            <a class="nav-link" href="{{ route('product.index') }}">
-                <i class="fas fa-fw fa-prescription-bottle-alt"></i>
-                <span>Items</span></a>
-        </li>
-        @endif
+    <!-- Nav Item - Items Menu -->
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('product.index') }}">
+            <i class="fas fa-fw fa-prescription-bottle-alt"></i>
+            <span>Items</span></a>
+    </li>
+    @endif
 
-        <li class="nav-item">
-            <a class="nav-link" href="{{ route('payment.history') }}">
-                <i class="fas fa-fw fa-list"></i>
-                <span>payment History</span></a>
-        </li>
-        <hr class="sidebar-divider">
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('payment.history') }}">
+            <i class="fas fa-fw fa-list"></i>
+            <span>payment History</span></a>
+    </li>
+    <hr class="sidebar-divider">
     <!-- Divider -->
 
     <!-- Sidebar Toggler (Sidebar) -->
